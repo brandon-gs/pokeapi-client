c9ee74640dfc72947d6a564bb2949f4468b462ca
BrandonGS <brandon.garcia@atomic32.com>
2022-08-10 02:03:37 -0500

    Add express server

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const path = require("path");
+const port = process.env.PORT || 8080;
+const app = express();
+
+app.use(express.favicon(path.join(__dirname, "build", "favicon.ico")));
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, "build")));
+
+app.get("/ping", (req, res) => {
+  return res.send("pong");
+});
+
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+app.listen(port);
